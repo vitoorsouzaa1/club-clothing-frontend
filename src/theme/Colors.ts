675385f567f897cb8c1e016e8e51581e2e8585ae
vitoorsouzaa1 <vitor.souza407@yahoo.com.br>
2022-12-25 23:46:46 -0300

    feat: add ThemeColors file

---
+++ src/theme/Colors.ts
@@ -0,0 +1,16 @@
+export const Colors = {
+  primary: '#212529',
+  text: {
+    white: '#F8F9FA',
+    dark: '#343a40'
+  },
+  background: {
+    dark: '#212529',
+    white: '#F8F9FA'
+  },
+  input: {
+    background: '#e9ecef',
+    placeholder: '#6c757d'
+  },
+  error: '#ff6a6a'
+}
