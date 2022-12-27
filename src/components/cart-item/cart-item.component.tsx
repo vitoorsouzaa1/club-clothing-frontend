bd1b625fca30b1d914244b2bf540f85c9e0584ff
vitoorsouzaa1 <vitor.souza407@yahoo.com.br>
2022-12-27 17:00:11 -0300

    feat: add Cart Item Component

---
+++ src/components/cart-item/cart-item.component.tsx
@@ -0,0 +1,41 @@
+import { FunctionComponent } from 'react'
+import { AiOutlinePlus, AiOutlineMinus, AiOutlineClose } from 'react-icons/ai'
+
+// Utilities
+import { ICartProduct } from '../../types/cart.types'
+
+// Styles
+import {
+  CartItemContainer,
+  CartItemImage,
+  CartItemInfo,
+  CartItemQuantity,
+  RemoveButton
+} from './cart-item.styles'
+
+interface ICartItemProps {
+  product: ICartProduct
+}
+
+export const CartItem: FunctionComponent<ICartItemProps> = ({ product }) => {
+  return (
+    <CartItemContainer>
+      <CartItemImage imageUrl={product.imageUrl} />
+
+      <CartItemInfo>
+        <p>{product.name}</p>
+        <p>{product.price}</p>
+
+        <CartItemQuantity>
+          <AiOutlineMinus size={20} />
+          <p>{product.quantity}</p>
+          <AiOutlinePlus size={20} />
+        </CartItemQuantity>
+      </CartItemInfo>
+
+      <RemoveButton>
+        <AiOutlineClose size={25} />
+      </RemoveButton>
+    </CartItemContainer>
+  )
+}
--- src/components/cart/cart.component.tsx
+++ src/components/cart/cart.component.tsx
@@ -3,6 +3,7 @@ import { BsCartCheck } from 'react-icons/bs'
 
 // Utilities
 import { CartContext } from '../../context/cart.context'
+import { CartItem } from '../cart-item/cart-item.component'
 
 // Components
 import { CustomButton } from '../custom-button/custom-button.component'
@@ -17,7 +18,7 @@ import {
 } from './cart.styles'
 
 export const CartComponent: FunctionComponent = () => {
-  const { isVisible, toggleCart } = useContext(CartContext)
+  const { isVisible, products, toggleCart } = useContext(CartContext)
 
   return (
     <CartContainer isVisible={isVisible}>
@@ -25,7 +26,9 @@ export const CartComponent: FunctionComponent = () => {
       <CartContent>
         <CartTitle>Cart</CartTitle>
 
-        {/* products */}
+        {products.map((product) => (
+          <CartItem key={product.id} product={product} />
+        ))}
         <CartTotal>Total: U$ 888</CartTotal>
 
         <CustomButton startIcon={<BsCartCheck />}>Checkout</CustomButton>
