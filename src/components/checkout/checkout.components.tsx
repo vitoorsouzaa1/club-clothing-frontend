ca9987bce319dc00551aab0a51df6ea138533147
Vitor Souza <vitor.souza407@yahoo.com.br>
2022-12-28 11:38:45 -0300

    feat: add strip api integration to chckeout

--- src/components/checkout/checkout.components.tsx
+++ src/components/checkout/checkout.components.tsx
@@ -1,10 +1,14 @@
-import { FunctionComponent, useContext } from 'react'
+import axios from 'axios'
+import { FunctionComponent, useContext, useState } from 'react'
 import { BsBagCheck } from 'react-icons/bs'
 
 // Utilities
 import { CartContext } from '../../context/cart.context'
+
+// Components
 import { CartItem } from '../cart-item/cart-item.component'
 import { CustomButton } from '../custom-button/custom-button.component'
+import { LoadingComponent } from '../loading/loading.components'
 
 // Styles
 import {
@@ -17,8 +21,29 @@ import {
 export const Checkout: FunctionComponent = () => {
   const { products, productsTotalPrice } = useContext(CartContext)
 
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleFinishPurchaseClick = async () => {
+    try {
+      setIsLoading(true)
+      const { data } = await axios.post(
+        `${process.env.REACT_APP_API_URL}/create-checkout-session`!,
+        {
+          products
+        }
+      )
+
+      window.location.href = data.url
+    } catch (e) {
+      console.log(e)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
   return (
     <CheckoutContainer>
+      {isLoading && <LoadingComponent />}
       <CheckoutTitle>Checkout</CheckoutTitle>
 
       {products.length > 0 ? (
@@ -31,7 +56,11 @@ export const Checkout: FunctionComponent = () => {
 
           <CheckoutTotal>Total: R${productsTotalPrice}</CheckoutTotal>
 
-          <CustomButton startIcon={<BsBagCheck />}>Buy</CustomButton>
+          <CustomButton
+            startIcon={<BsBagCheck />}
+            onClick={handleFinishPurchaseClick}>
+            Buy
+          </CustomButton>
         </>
       ) : (
         <p>Your cart is empty</p>
