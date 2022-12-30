a828975536bc41e9206081740773faa281ff8f76
Vitor Souza <vitor.souza407@yahoo.com.br>
2022-12-30 11:13:10 -0300

    refactor: change what to expect from the test

--- src/components/custom-button/custom-button.spec.tsx
+++ src/components/custom-button/custom-button.spec.tsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react'
 import { CustomButton } from './custom-button.component'
 
 describe('Custom Button', () => {
-  test('should render with correct children', () => {
+  test('should render with correct values', () => {
     const { getByText } = render(<CustomButton>lorem ipsum</CustomButton>)
 
     getByText('lorem ipsum')
