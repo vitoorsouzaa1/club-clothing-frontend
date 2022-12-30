962dae1941c3b4e36143c612dc888e9c6d57f601
Vitor Souza <vitor.souza407@yahoo.com.br>
2022-12-30 11:13:58 -0300

    test: ensure CustomInputComponent render with error if hasError is true

---
+++ src/components/custom-input/custom-input.spec.tsx
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/react'
+import { Colors } from '../../theme/Colors'
+import { CustomInput } from './custom-input.component'
+
+describe('Custom Input', () => {
+  test('should render with error if hasError is true', () => {
+    const { getByPlaceholderText } = render(
+      <CustomInput placeholder="lorem ipsum" hasError={true} />
+    )
+
+    const input = getByPlaceholderText('lorem ipsum')
+
+    expect(input).toHaveStyle({ border: `2px solid ${Colors.error}` })
+  })
+})
